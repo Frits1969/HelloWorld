a167012c269e92e4bc76f48c7c78d48dadcba0a1
Frits van Leeuwen <fwtvanleeuwen@gmail.com>
2025-05-14 12:13:17 +0200

    Add files via upload

---
+++ modules/helloworld/translations/en.php
@@ -0,0 +1,5 @@
+<?php
+return [
+    'Hello World' => 'Hello World',
+    'A simple module to test translations.' => 'A simple module to test translations.',
+];
---
+++ modules/helloworld/translations/nl.php
@@ -0,0 +1,5 @@
+<?php
+return [
+    'Hello World' => 'Hallo Wereld',
+    'A simple module to test translations.' => 'Een eenvoudige module om vertalingen te testen.',
+];
